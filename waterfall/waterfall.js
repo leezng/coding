44cc948a519248549aec64be600885f26adc69d4
leezng <294795201@qq.com>
2018-02-02 21:44:47 +0800

    waterfall优化

--- waterfall/waterfall.js
+++ waterfall/waterfall.js
@@ -4,6 +4,7 @@ class Waterfall {
     this.grid = grid;
     this.gridWidth = this.container.offsetWidth / this.grid;
     this.heightArr = []; // 保存当前所有列的高度
+    this.gutter = 20
   }
 
   append (imgs) {
@@ -11,23 +12,23 @@ class Waterfall {
   }
 
   onload (box, image) {
-    let index
+    let { left, top } = this.getPosition();
+    box.style.position = 'absolute';
+    box.style.left = `${left}px`;
+    box.style.top = `${top}px`;
     box.style.width = this.gridWidth + 'px';
-    if (this.heightArr.length === this.grid) {
-      let { left, top } = this.getPosition();
-      box.style.position = 'absolute';
-      box.style.left = `${left}px`;
-      box.style.top = `${top}px`;
-      index = left / this.gridWidth;
-    }
+    box.style.boxSizing = 'border-box';
+    box.style.padding = `${this.gutter / 2}px`;
+
     this.container.appendChild(box);
-    this.increaseHeightArr(image.height + 20, index);
+    this.increaseHeightArr(image.height + this.gutter, left / this.gridWidth);
   }
 
   getPosition () {
-    let minBoxHeight = Math.min(...this.heightArr);
+    const isRowOne = this.heightArr.length < this.grid // 若数组长度还未达到列数，说明此时还应排在第一行
+    const minBoxHeight = isRowOne ? 0 : Math.min(...this.heightArr);
     return {
-      left: this.heightArr.indexOf(minBoxHeight) * this.gridWidth,
+      left: this.gridWidth * (isRowOne ? this.heightArr.length : this.heightArr.indexOf(minBoxHeight)),
       top: minBoxHeight
     };
   }
@@ -40,12 +41,14 @@ class Waterfall {
     img.onload = () => this.onload(box, img);
     img.onerror = () => {}
     img.src = src;
+    img.style.width = '100%';
+    img.style.height = 'auto';
     return box;
   }
 
   increaseHeightArr (height, index) {
-    if (index === undefined) {
-      this.heightArr.push(height);
+    if (isNaN(this.heightArr[index])) {
+      this.heightArr[index] = height;
     } else {
       this.heightArr[index] += height
     }
